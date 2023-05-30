834784dfc69e2c94f24c8e1d89ca14c2ea8dd7cc
Joel Merino <joel@vauxoo.com>
2023-05-30 17:02:50 -0600

    [ADD] academy: add new module

---
+++ acamedy/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Academy B&F",
+    "version": "16.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "Website",
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "demo": [],
+    "application": True,
+    "installable": True,
+}
