fac75d6dbbedafbee9ef0d6bb6dff7f35f603279
Joel Merino <joel@vauxoo.com>
2023-06-03 17:01:55 -0600

    [ADD] theme_tutorial: added a new module

---
+++ theme_tutorial/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Theme Tutorial",
+    "description": "A theme tutorial for Odoo 16.0",
+    "version": "16.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "Theme/Creative",
+    "depends": [
+        "website",
+    ],
+    "data": [],
+}
