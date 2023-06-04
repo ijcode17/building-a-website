87de2995e8b40946094e29dc38c6be15e2076ff3
Joel Merino <joel@vauxoo.com>
2023-06-04 00:22:13 -0600

    [ADD] theme_tutorial: added an odoo page

--- theme_tutorial/__manifest__.py
+++ theme_tutorial/__manifest__.py
@@ -1,11 +1,15 @@
 {
-    "name": "Theme Tutorial",
+    "name": "Vauxoo Theme Tutorial",
     "description": "A theme tutorial for Odoo 16.0",
     "version": "16.0.1.0.0",
     "author": "Vauxoo",
     "category": "Theme/Creative",
+    "license": "LGPL-3",
     "depends": [
         "website",
     ],
-    "data": [],
+    "data": [
+        "views/layout.xml",
+    ],
+    "installable": True,
 }
